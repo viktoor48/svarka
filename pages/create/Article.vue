ff0d302422073ff09a4f7504343048155f3b2135
Виктор <kosenkov@intaro.email>
2024-04-05 01:20:08 +0300

    updated

--- pages/create/Article.vue
+++ pages/create/Article.vue
@@ -1,78 +1,3 @@
-<template>
-  <div class="mx-auto bg-[#f9faff] w-full">
-    <div class="p-5 mx-auto">
-      <LoadingSpinner v-if="isLoading" />
-      <div v-else class="container">
-        <h2 class="text-2xl font-bold">Создание новой записи</h2>
-        <div class="flex flex-col gap-2 mt-5">
-          <label class="text-lg" for="category">Выберите категорию</label>
-          <div>
-            <select
-              v-if="getCategorys"
-              v-model="selectedCategory"
-              name="category"
-              id="categorySelect"
-              class="outline-none bg-gray-100 border border-[#ccc] rounded-md py-1 px-3"
-            >
-              <option
-                v-for="category in getCategorys"
-                :key="category.id"
-                :value="category.id"
-              >
-                {{ category.name }}
-              </option>
-            </select>
-          </div>
-          <div class="flex flex-col gap-2">
-            <div class="flex items-baseline"><span class="text-orange-300 text-5xl">1</span>-й блок</div>
-            <form id="first-block" class="flex flex-col gap-5">
-                <textarea v-model="article.text" class="border border-gray-300 p-2 hover:border-gray-500 active:border-gray-500 outline-none"/>
-                <div>
-                    <input type="file" class="" name="img-first-block" @change="uploadImage($event)">
-                    <!-- Загрузка картинки в папку public/assets/images/articles-->
-                </div>
-                <!-- превью картинки -->
-            </form>
-            
-            <div class="flex flex-col gap-5">
-              <div v-for="(block, index) in blocks" :key="index">
-                <div class="flex items-baseline"><span class="text-orange-300 text-5xl">{{ index+2 }}</span>-й блок</div>
-                <div v-if="block.type === 'text'">
-                    <textarea class="border w-full border-gray-300 p-2 hover:border-gray-500 active:border-gray-500 outline-none" cols="30" rows="5" v-model="block.content"></textarea>
-                </div>
-                <div v-else class="flex flex-col gap-3">
-                    <label :for="`block-${index}`">
-                        Загрузите картинку
-                    </label>
-                    <div>
-                        <input type="file" :name="`block-${index}`" @change="handleBlockImageUpload($event, index)">
-                        <!-- загрузка картинки в папку public/assets/images/blocks -->
-                    </div>
-                    <!-- превью картинки -->
-                </div>
-              </div>
-            </div>
-            <div class="flex gap-5 flex-wrap">
-              <button
-                @click="createBlock('text')"
-                class="py-2 px-5 text-lg bg-yellow rounded-2xl duration-300 hover:scale-105"
-              >
-                Создать текстовый блок
-              </button>
-              <button
-                @click="createBlock('image')"
-                class="py-2 px-5 text-lg bg-yellow rounded-2xl duration-300 hover:scale-105"
-              >
-                Создать блок с картинкой
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script setup>
 import { ref, computed, watch } from "vue";
 import { useStore } from "~/stores";
@@ -115,9 +40,9 @@ const getCategorys = computed(() => {
 
 const createBlock = (type) => {
   if (type === "text") {
-    blocks.value.push({ type: "text", content: "Новый текстовый блок" });
+    blocks.value.push({ type: "text", content: "Новый текстовый блок", title: '', });
   } else if (type === "image") {
-    blocks.value.push({ type: "image", content: "Путь к изображению" });
+    blocks.value.push({ type: "image", content: "Путь к изображению", title: '', });
   }
 };
 
@@ -147,15 +72,153 @@ const handleBlockImageUpload = (event, index) => {
   }
 };
 
-//рабочее временное решение
-const uploadImage = async (event) => {
-    const file = event.target.files[0];
-    const directoryHandle = await window.showDirectoryPicker();
-    const fileHandle = await directoryHandle.getFileHandle(file.name, { create: true });
-    const writable = await fileHandle.createWritable();
-    await writable.write(file);
-    await writable.close();
+//  /assets/images/articles/name
+const uploadImageArticle = async (event) => {
+  const file = event.target.files[0];
+  const directoryHandle = await window.showDirectoryPicker(); //чтение файлов
+  const fileHandle = await directoryHandle.getFileHandle(file.name, {
+    create: true,
+  });
+  const writable = await fileHandle.createWritable();
+  await writable.write(file);
+  await writable.close();
+  console.log("Имя файла:", fileHandle.name);
+  article.value.image = `/assets/images/forNewArticles/${fileHandle.name}`;
 };
+
+// /assets/images/blocks/name
+const uploadImageBlock = async (event, index) => {
+  const file = event.target.files[0];
+  const directoryHandle = await window.showDirectoryPicker(); //чтение файлов
+  const fileHandle = await directoryHandle.getFileHandle(file.name, {
+    create: true,
+  });
+  const writable = await fileHandle.createWritable();
+  await writable.write(file);
+  await writable.close();
+  console.log("Имя файла:", fileHandle.name);
+  blocks.value[index].image = `/assets/images/forNewArticles/${fileHandle.name}`;
+};
+
+const save = async () => {
+  let formData = {
+    categoryId: selectedCategory.value,
+    article: article.value,
+    blocks: blocks.value,
+  }
+  
+  console.log(formData);
+  const response = await store.createNewArticle(formData);
+  console.log(response);
+  await router.push("/");
+}
 </script>
+
+<template>
+  <div class="mx-auto bg-[#f9faff] w-full">
+    <div class="p-5 mx-auto">
+      <LoadingSpinner v-if="isLoading" />
+      <div v-else class="container">
+        <h2 class="text-2xl font-bold">Создание новой записи</h2>
+        <div class="flex flex-col gap-2 mt-5">
+          <label class="text-lg" for="category">Выберите категорию</label>
+          <div>
+            <select
+              v-if="getCategorys"
+              v-model="selectedCategory"
+              name="category"
+              id="categorySelect"
+              class="outline-none bg-gray-100 border border-[#ccc] rounded-md py-1 px-3"
+            >
+              <option
+                v-for="category in getCategorys"
+                :key="category.id"
+                :value="category.id"
+              >
+                {{ category.name }}
+              </option>
+            </select>
+          </div>
+          <div class="flex flex-col gap-2">
+            <div class="flex items-baseline">
+              <span class="text-orange-300 text-5xl">1</span>-й блок
+            </div>
+            <form id="first-block" class="flex flex-col gap-5">
+              <textarea
+                v-model="article.text"
+                class="border border-gray-300 p-2 hover:border-gray-500 active:border-gray-500 outline-none"
+              />
+              <div>
+                <input
+                  type="file"
+                  class=""
+                  name="img-first-block"
+                  @change="uploadImageArticle($event)"
+                />
+              </div>
+              <div>
+                <img v-show="article.image" :src="article.image" />
+              </div>
+            </form>
+
+            <div class="flex flex-col gap-5">
+              <div v-for="(block, index) in blocks" :key="index">
+                <div class="flex items-baseline">
+                  <span class="text-orange-300 text-5xl">{{ index + 2 }}</span
+                  >-й блок
+                </div>
+                <div v-if="block.type === 'text'" class="flex flex-col gap-3">
+                  <label :for="`block-title-${index}`">Заголовок</label>
+                  <input type="text" :name="`block-title-${index}`" v-model="block.title" class="border w-full border-gray-300 p-2 hover:border-gray-500 active:border-gray-500 outline-none">
+                  <textarea
+                    class="border w-full border-gray-300 p-2 hover:border-gray-500 active:border-gray-500 outline-none"
+                    cols="30"
+                    rows="5"
+                    v-model="block.content"
+                  ></textarea>
+                </div>
+                <div v-else class="flex flex-col gap-3">
+                  <label :for="`block-title-${index}`">Заголовок</label>
+                  <input type="text" :name="`block-title-${index}`" v-model="block.title" class="border w-full border-gray-300 p-2 hover:border-gray-500 active:border-gray-500 outline-none">
+                  <label :for="`block-${index}`"> Загрузите картинку </label>
+                  <div>
+                    <input
+                      type="file"
+                      :name="`block-${index}`"
+                      @change="uploadImageBlock($event, index)"
+                    />
+                  </div>
+                  <div>
+                    <img v-show="block.image" :src="block.image" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="flex gap-5 flex-wrap">
+              <button
+                @click="createBlock('text')"
+                class="py-2 px-5 text-lg bg-yellow rounded-2xl duration-300 hover:scale-105"
+              >
+                Создать текстовый блок
+              </button>
+              <button
+                @click="createBlock('image')"
+                class="py-2 px-5 text-lg bg-yellow rounded-2xl duration-300 hover:scale-105"
+              >
+                Создать блок с картинкой
+              </button>
+              <button
+                @click="save()"
+                class="py-2 px-5 text-lg bg-orange-300 rounded-2xl duration-300 hover:scale-105"
+              >
+                Сохранить
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style scoped></style>
--- stores/index.js
+++ stores/index.js
@@ -1,108 +1,139 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useStore = defineStore('product', {
-    state: () => {
-        return {
-            user: localStorage.getItem('user') || null,
-            categories: [],
-            currentCategory: null,
-            articles: null,
-            currentAtricle: null,
+export const useStore = defineStore("product", {
+  state: () => {
+    return {
+      user: localStorage.getItem("user") || null,
+      categories: [],
+      currentCategory: null,
+      articles: null,
+      currentAtricle: null,
+    };
+  },
+  actions: {
+    async fetchCurrentCategory(id) {
+      //рабочая
+      try {
+        const response = await fetch(
+          `http://localhost:8000/api/categoriess/${id}`
+        );
+        const category = await response.json();
+        this.currentCategory = category;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async fetchCurrentArticle(categoryId, articleId) {
+      //рабочая
+      try {
+        const response = await fetch(
+          `http://localhost:8000/category/${categoryId}/article/${articleId}`
+        );
+        const article = await response.json();
+        this.currentAtricle = article;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async fetchCategories() {
+      //рабочая
+      try {
+        const response = await fetch("http://localhost:8000/api/categoriess");
+        const categories = await response.json();
+        this.categories = categories;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async fetchArticles(id) {
+      //рабочая //контроллер
+      try {
+        const response = await fetch(`http://localhost:8000/category/${id}`);
+        const articles = await response.json();
+        this.articles = articles;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async createNewArticle(formData) {
+      try {
+        const url = "http://localhost:8000/create/new/article";
+
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+
+        console.log('response:');
+        console.log(response);
+
+        if (!response.ok) {
+          // Если ответ не успешен, обработка ошибки
+          throw new Error(`HTTP error! status: ${response.status}`);
         }
+
+        const responseData = await response.json(); // Парсим ответ как JSON
+        return responseData;
+      } catch (error) {
+        console.error("Ошибка при сохранении данных:", error);
+        throw error;
+      }
     },
-    actions: {
-        async fetchCurrentCategory(id) {
-            //рабочая
-            try {
-                const response = await fetch(`http://localhost:8000/api/categoriess/${id}`);
-                const category = await response.json();
-                this.currentCategory = category;
-            } catch (e) {
-                console.log(e);
-            }
-        },
-        async fetchCurrentArticle(categoryId, articleId) {
-            //рабочая
-            try {
-                const response = await fetch(`http://localhost:8000/category/${categoryId}/article/${articleId}`);
-                const article = await response.json();
-                this.currentAtricle = article;
-            } catch (e) {
-                console.log(e);
-            }
-        },
-        async fetchCategories() {
-            //рабочая
-            try {
-                const response = await fetch('http://localhost:8000/api/categoriess');
-                const categories = await response.json();
-                this.categories = categories;
-            } catch (e) {
-                console.log(e);
-            }
-        },
-        async fetchArticles(id) {
-            //рабочая //контроллер
-            try {
-                const response = await fetch(`http://localhost:8000/category/${id}`);
-                const articles = await response.json();
-                this.articles = articles;
-            } catch (e) {
-                console.log(e);
-            }
-        },
-        async onLogin(email, password) {
-            try {
-                const url = 'http://localhost:8000/login';
-                const data = {
-                    email: email,
-                    password: password
-                };
-                console.log(email, password);
-                
-                const response = await fetch(url, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(data)
-                });
-        
-                if (!response.ok) {
-                    // Если ответ не успешен, обработка ошибки
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-        
-                const user = await response.json();
-                
-                // Сохраняем пользователя в хранилище
-                this.user = user;
-                localStorage.setItem('user', JSON.stringify(this.user));
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        },
-        logout() {
-            this.user = null;
-            localStorage.removeItem('user');
-            console.log(this.user)
-        },
+    async onLogin(email, password) {
+      try {
+        const url = "http://localhost:8000/login";
+        const data = {
+          email: email,
+          password: password,
+        };
+        console.log(email, password);
+
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+
+        if (!response.ok) {
+          // Если ответ не успешен, обработка ошибки
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const user = await response.json();
+
+        // Сохраняем пользователя в хранилище
+        this.user = user;
+        localStorage.setItem("user", JSON.stringify(this.user));
+      } catch (error) {
+        console.error("Error:", error);
+      }
     },
-    getters: {
-        getCurrentCategory(state) {
-            return state.currentCategory;
-        },
-        getCategories(state) {
-            return state.categories;
-        },
-        getArticles(state) {
-            return state.articles;
-        },
-        getCurrentArticle(state) {
-            return state.currentAtricle;
-        },
-        getUser(state) {
-            return state.user;
-        },
+    logout() {
+      this.user = null;
+      localStorage.removeItem("user");
+      console.log(this.user);
     },
-})
+  },
+  getters: {
+    getCurrentCategory(state) {
+      return state.currentCategory;
+    },
+    getCategories(state) {
+      return state.categories;
+    },
+    getArticles(state) {
+      return state.articles;
+    },
+    getCurrentArticle(state) {
+      return state.currentAtricle;
+    },
+    getUser(state) {
+      return state.user;
+    },
+  },
+});
